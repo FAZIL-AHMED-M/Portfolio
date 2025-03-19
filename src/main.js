37c49f562f258bb733b371c9d7909967557aa85a
fazil <fazilgamingdp@gmail.com>
2025-03-19 21:50:36 +0530

    about sec

--- src/main.js
+++ src/main.js
@@ -1,3 +1,38 @@
 import '../styles/style.css';
 import '../styles/components/header.css';
 import '../styles/components/hero.css'
+import '../styles/components/about.css'
+
+
+/* 
+@media (min-width: 475px) {
+ 
+}
+
+
+@media (min-width: 640px) {
+ 
+}
+
+
+@media (min-width: 768px) {
+ 
+}
+
+
+@media (min-width: 1024px) {
+  
+}
+
+
+@media (min-width: 1280px) {
+  
+}
+
+
+
+@media (min-width: 1536px) {
+
+} */
+
+
