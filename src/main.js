68dd3ea2243f67d35f1caac764f5c09bb596ede3
fazil <fazilgamingdp@gmail.com>
2025-03-20 20:27:37 +0530

    about comp

--- src/main.js
+++ src/main.js
@@ -1,38 +1,9 @@
 import '../styles/style.css';
 import '../styles/components/header.css';
-import '../styles/components/hero.css'
 import '../styles/components/about.css'
-
-
-/* 
-@media (min-width: 475px) {
- 
-}
-
-
-@media (min-width: 640px) {
- 
-}
-
-
-@media (min-width: 768px) {
- 
-}
-
-
-@media (min-width: 1024px) {
-  
-}
-
-
-@media (min-width: 1280px) {
-  
-}
+import '../styles/components/hero.css'
 
 
 
-@media (min-width: 1536px) {
-
-} */
 
 
