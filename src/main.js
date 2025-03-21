0c55e80f143d2d2fe68bb7015f942e8a92df4806
fazil <fazilgamingdp@gmail.com>
2025-03-21 23:55:50 +0530

    contact completed

--- src/main.js
+++ src/main.js
@@ -3,6 +3,8 @@ import '../styles/components/header.css';
 import '../styles/components/about.css'
 import '../styles/components/hero.css'
 import '../styles/components/projects.css'
+import '../styles/components/contact.css';
+import '../styles/components/footer.css'
 
 
 
