b51f5235bf534d72a41422a0323031573ae92a93
fazil <fazilahmed.m1234@gmail.com>
2025-03-23 10:50:40 +0530

    bug fixed

--- src/utils/mobilemenu.js
+++ src/utils/mobilemenu.js
@@ -2,18 +2,20 @@ const mobilenav = () => {
 const hammenu = document.querySelector('.ham');
 const mobilenav = document.querySelector('.mob-nav')
 const mobilelinks = document.querySelectorAll ('.mobile-menu')
+
+
 let ismobilenavOpen = false;
 
 hammenu.addEventListener('click', () => {
  
  ismobilenavOpen = !ismobilenavOpen;
 
- if (ismobilenavOpen === true) {
+ if (ismobilenavOpen) {
  mobilenav.style.display = 'flex'
  document.body.style.overflowY = 'hidden'
  } else {
    mobilenav.style.display = 'none'
- document.body.style.overflowY = 'hidden'
+ document.body.style.overflowY = 'auto'
 
  }
 })
